3bfe44553e3efd571d0b222f3125401d42660d42
Pedro Urbina <pedro.urbina@gmail.com>
2015-10-13 20:42:33 -0400

    Functional views with full CRUD

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,5 +2,8 @@ class Wiki < ActiveRecord::Base
 	
   belongs_to :user
 
+  validates :title, length: {minimum: 3}, presence: true
+  validates :body, length: {minimum: 10}, presence: true
+
   scope :visible_to, -> (user){user ? all : where(public: true)}
 end
