baca2ed922bec76aff4249f7e678c76a0b3c7864
Pedro Urbina <pedro.urbina@gmail.com>
2015-10-18 13:09:04 -0400

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do
+	Wiki.create!(
+		title: Faker::Lorem.sentence,
+		body: Faker::Lorem.paragraph)
+end
+
+wikis = Wiki.all
+
+5.times do
+	User.create!(
+		email: Faker::Internet.email,
+		password: Faker::Lorem::characters(8))
+end
+
+users = User.all
+
+puts "Seed finished"
+puts "#{Wiki.count} wikis created"
+puts "#{User.count} users created"
