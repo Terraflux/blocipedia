bda4f62268f96423f699921bf21abd21ce41332e
Pedro Urbina <pedro.urbina@gmail.com>
2015-10-20 12:27:21 -0400

    before_save method replaced, database reset errors

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,10 +2,19 @@ class Wiki < ActiveRecord::Base
 	
   belongs_to :user
 
-  before_create {self.private ||= false}
+  before_save :default_to_public
 
   validates :title, length: {minimum: 3}, presence: true
   validates :body, length: {minimum: 10}, presence: true
 
   scope :visible_to, -> (user){(user.admin? || user.premium?) ? all : where(private: false)}
+
+
+
+  private
+
+  def default_to_public
+  	self.private ||= false
+  end
+
 end
